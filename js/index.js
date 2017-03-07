c5d2e8cd3714c218aecd4d133388464dfeb30ecc
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-03-07 17:49:27 -0500

    added basic css, app is fully functional

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
-window.addEventListener ('keydown', function(e){
+
+function playSound(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if(!audio) return; // stops the function from running incase wrong key was pressed.
+  audio.currentTime = 0; //rewinds the tape playing
   audio.play();
-})
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener ('keydown', playSound);
